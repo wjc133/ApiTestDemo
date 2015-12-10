af2e277d9494b47d57f3f7fb72e886c66bff309d
wjc133 <wjc133@sina.com>
2015-12-10 21:17:01 +0800

    add sth. lost....

---
+++ src/main/java/com/elite/api/web/RunningController.java
@@ -0,0 +1,12 @@
+package com.elite.api.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by wjc133
+ * Date: 2015/12/10
+ * Time: 21:14
+ */
+@Controller
+public class RunningController {
+}
---
+++ src/test/java/com/elite/api/RunningTest.java
@@ -0,0 +1,9 @@
+package com.elite.api;
+
+/**
+ * Created by wjc133
+ * Date: 2015/12/10
+ * Time: 21:13
+ */
+public class RunningTest {
+}
